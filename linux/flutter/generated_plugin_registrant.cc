52294efcf7e1f8fdc0fafc4dcc235f8d554812ce
Hadi Azarnasab <hazard7546@gmail.com>
2022-06-01 17:23:02 +0430

    feat: auto deploy to heroku with github action

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -8,6 +8,7 @@
 
 #include <file_selector_linux/file_selector_plugin.h>
 #include <image_compression_flutter/image_compression_flutter_plugin.h>
+#include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) file_selector_linux_registrar =
@@ -16,4 +17,7 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) image_compression_flutter_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "ImageCompressionFlutterPlugin");
   image_compression_flutter_plugin_register_with_registrar(image_compression_flutter_registrar);
+  g_autoptr(FlPluginRegistrar) sqlite3_flutter_libs_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "Sqlite3FlutterLibsPlugin");
+  sqlite3_flutter_libs_plugin_register_with_registrar(sqlite3_flutter_libs_registrar);
 }
